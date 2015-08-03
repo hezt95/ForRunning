c8e6d3ce735abdd4764d61e3330cb5e9825aae10
hezt95 <hezt95@gmail.com>
2015-08-03 23:13:07 +0800

    Unfinished layer

--- ForRunning/HZTRoundProgressBarView.swift
+++ ForRunning/HZTRoundProgressBarView.swift
@@ -17,49 +17,45 @@ class HZTRoundProgressBarView: UIView {
         parentView.addSubview(self)
         self.backgroundColor = UIColor.lineColor()
         self.setupColorLayer()
+        self.setupColorMaskLayer()
+        self.setupBGColorMaskLayer()
+        self.backgroundColorMaskLayer!.strokeEnd = 0.34
     }
     
     func setupColorLayer() {
         colorLayer = CAShapeLayer()
         colorLayer!.frame = self.bounds
-        println("colorLayer frame: " + String(stringInterpolationSegment: colorLayer!.frame))
-        println("colorLayer bounds: " + String(stringInterpolationSegment: colorLayer!.bounds))
-        println("view frame: " + String(stringInterpolationSegment: self.frame))
-        println("view bounds: " + String(stringInterpolationSegment: self.bounds))
-        println("self.layer frame: " + String(stringInterpolationSegment: self.layer.frame))
-        println("self.layer bounds: " + String(stringInterpolationSegment: self.layer.bounds))
         self.layer.addSublayer(colorLayer)
         
         let leftLayer = CAGradientLayer()
-        leftLayer.frame = CGRectMake(colorLayer!.frame.origin.x, colorLayer!.frame.origin.y, colorLayer!.frame.width / 2, colorLayer!.frame.height)
-        println("leftLayer frame: " + String(stringInterpolationSegment: leftLayer.frame))
-        leftLayer.locations = [0.3, 0.9, 1.0]
+        leftLayer.frame = CGRectMake(colorLayer!.bounds.origin.x, colorLayer!.bounds.origin.y, colorLayer!.bounds.width / 2, colorLayer!.bounds.height)
+        leftLayer.locations = [0.2, 0.9, 1.0]
         leftLayer.colors = [UIColor.yellowColor().CGColor, UIColor.greenColor().CGColor]
         colorLayer!.addSublayer(leftLayer)
         
         let rightLayer = CAGradientLayer()
-        rightLayer.frame = CGRectMake(colorLayer!.frame.origin.x + colorLayer!.frame.width / 2, colorLayer!.frame.origin.y, colorLayer!.frame.width / 2, colorLayer!.frame.height)
-        println("rightLayer frame: " + String(stringInterpolationSegment: rightLayer.frame))
-        leftLayer.locations = [0.3, 0.9, 1.0]
+        rightLayer.frame = CGRectMake(colorLayer!.bounds.origin.x + colorLayer!.bounds.width / 2, colorLayer!.bounds.origin.y, colorLayer!.bounds.width / 2, colorLayer!.bounds.height)
+        rightLayer.locations = [0.2, 0.9, 1.0]
         rightLayer.colors = [UIColor.yellowColor().CGColor, UIColor.redColor().CGColor]
         colorLayer!.addSublayer(rightLayer)
     }
 
     func generateMaskLayer() -> CAShapeLayer {
+        let roundLineWidth = 15
         let layer = CAShapeLayer()
-        layer.frame = self.frame
-        let path = UIBezierPath(arcCenter: self.center, radius: self.frame.width / 2, startAngle: CGFloat(0), endAngle: CGFloat(2 * π), clockwise: true)
-        layer.lineWidth = 15
+        layer.frame = self.bounds
+        let path = UIBezierPath(arcCenter: CGPointMake(self.frame.width / 2, self.frame.height / 2), radius: self.frame.width / 2 - CGFloat(roundLineWidth) / 2, startAngle: CGFloat(1.5 * π), endAngle: CGFloat(3.5 * π), clockwise: true)
+        layer.lineWidth = CGFloat(roundLineWidth)
         layer.path = path.CGPath
         layer.fillColor = UIColor.clearColor().CGColor
-        layer.strokeColor = UIColor.lineColor().CGColor
+        layer.strokeColor = UIColor.blueColor().CGColor
         layer.lineCap = kCALineCapRound
         return layer
     }
     
     func setupColorMaskLayer() {
         let layer = generateMaskLayer()
-        layer.lineWidth = 20.5
+        layer.lineWidth = CGFloat(15.5)
         self.colorLayer!.mask = layer
         self.colorMaskLayer = layer
     }
